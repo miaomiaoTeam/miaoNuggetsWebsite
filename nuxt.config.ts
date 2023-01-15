609a87fec3fab52f526038c57762508eeeb50cd7
Angelna <1443329043@qq.com>
2023-01-15 19:06:37 +0800

    feat✨:  自动引入 pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
+  // modules: ["@nuxt/ui"],
+  modules: [
+    "@nuxt/ui",
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // 自动引入 `usePinia()`
+          "defineStore",
+          // 自动引入 `usePinia()` 并重命名为 `usePiniaStore()`
+          ["defineStore", "definePiniaStore"],
+        ],
+      },
+    ],
+  ],
   css: ["~/assets/css/main.css", "element-plus/dist/index.css"],
   postcss: {
     plugins: {
