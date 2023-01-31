b7a11329c9fa2f058b4e013dc6d4dfe1a8bfc8fa
MZ-Dlovely <93523064+MZ-Dlovely@users.noreply.github.com>
2023-01-31 23:26:58 +0800

    Feat tailwind screen miao dev (#9)
    * config🔧:  添加了tailwind断点配置
    
    - postLep: 1000px
    - postDes: 1140px
    
    * feat✨:  新增了环境类型，添加了对一般静态资源引入的类型提示

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {},
+    screens: {
+      postLep: '1000px',
+      postDes: '1140px',
+    },
+  },
+}
