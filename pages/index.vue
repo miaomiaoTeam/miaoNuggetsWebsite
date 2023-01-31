376b71a92dfe7197526503db5988e6ff3c9670d4
luzilin <744883907@qq.com>
2023-01-31 22:00:58 +0800

    style💄: 文章详情页mobile端基本样式

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <NuxtWelcome></NuxtWelcome>
-  <!-- <div><PagePreview /></div> -->
 </template>
 
 <script lang="ts" setup></script>
--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <p>{{ $route.params.id }}</p>
     <!-- 移动端 -->
-    <div>
-      <div class="w-[200px] h-40 bg-orange-300 md:w-[1000px]">dd</div>
-      <main-content>
-        <template #main>
-          <detail-content></detail-content>
-        </template>
-        <template #sideleft> </template>
-        <template #sidebar>
-          <ArticlelSidebar></ArticlelSidebar>
-          <directory-nav></directory-nav>
-        </template>
-      </main-content>
+    <div class="block md:hidden">
+      <post-detail-content></post-detail-content>
     </div>
     <!-- PC端-->
-    <div class="md:xxx"></div>
+    <div class="hidden md:block"></div>
   </div>
 </template>
+<script lang="ts" setup>
+const route = useRoute()
+console.log('route.params.id', route.params.id)
+</script>
