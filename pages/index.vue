5a41c3a6630287c112c6e5e196833c0123e6ed0e
whu125 <2691806221@qq.com>
2023-02-03 16:32:08 +0800

    fix🐛:  解决冲突

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,5 @@
 <template>
-<<<<<<< Updated upstream
   <NuxtWelcome></NuxtWelcome>
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <!-- <div><PagePreview /></div> -->
-=======
-=======
->>>>>>> f2a1642a7870e2c9c7a353634fe1d9be72b1114d
-=======
-  <!-- <NuxtWelcome></NuxtWelcome> -->
-  <div>test</div>
->>>>>>> Stashed changes
-<<<<<<< HEAD
->>>>>>> f2a1642 (feat✨:  layout-tab完成大部分)
-=======
->>>>>>> f2a1642a7870e2c9c7a353634fe1d9be72b1114d
 </template>
 
 <script lang="ts" setup></script>
