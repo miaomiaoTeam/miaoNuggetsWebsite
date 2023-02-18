bd86fa58692b8aa043903292e2d14b9ac20fe2b6
RyanFcr <3200103626@zju.edu.cn>
2023-02-19 00:00:06 +0800

    feat:✨右侧广告位以及作者

---
+++ components/home/AdBanner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-row>
+      <el-col v-for="item in itemms" :key="item.id" :span="8">
+        <nuxt-link :to="item.link">
+          <el-image :src="item.src" style="width: 240px" />
+        </nuxt-link>
+      </el-col>
+    </el-row>
+    <el-card style="width: 240px">
+      <el-row>
+        <el-col :span="12">
+          <el-image :src="CardImgSrc" />
+        </el-col>
+        <el-col :span="12">
+          <h3 style="font-weight: bold; font-size: larger">下载稀土掘金APP</h3>
+          <p>一个帮助开发者成长的社区</p>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+const itemms = ref([
+  { id: '1', src: '../../assets/ad.webp', link: '/' },
+  { id: '2', src: '../../assets/ad.webp', link: '/' },
+  { id: '3', src: '../../assets/ad.webp', link: '/' },
+])
+const CardImgSrc = '../../assets/ad.webp'
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
-<template>
-  <NuxtWelcome></NuxtWelcome>
-  <!-- <div><PagePreview /></div> -->
-</template>
+<script>
+import AuthorRank from '#components'
 
-<script lang="ts" setup></script>
+export default {
+  components: {
+    AuthorRank,
+    // AdBanner,
+  },
+}
+</script>
+<template>
+  <!-- <NuxtWelcome></NuxtWelcome> -->
+  <!-- <div><PagePreview /></div> -->
+  <!-- <AuthorRank /> -->
+  <div>
+    <AuthorRank />
+    <!-- <AdBanner /> -->
+  </div>
+</template>
