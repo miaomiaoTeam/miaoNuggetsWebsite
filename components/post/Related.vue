19de561d80ae053efced8ae6e4268cfa7dc0c479
luzilin <744883907@qq.com>
2023-02-19 10:17:26 +0800

    fix:  获取相关文章数据

--- components/post/Related.vue
+++ components/post/Related.vue
@@ -13,7 +13,7 @@
         <div
           class="text-[#8a919f] text-[14px] font-normal leading-[22px] mt-[4px]"
         >
-          {{ post.praise }}点赞 · {{ post.comment }}评论
+          {{ post.collect_count }}点赞 · {{ post.comment_count }}评论
         </div>
       </div>
     </el-card>
@@ -21,38 +21,62 @@
 </template>
 
 <script setup lang="ts">
-const posts = ref([
-  {
-    id: '1',
-    title: '字节跳动是如何落地微前端的',
-    praise: '356',
-    comment: '14',
-  },
-  {
-    id: '2',
-    title: '记笔记,赢大礼!笔记创作活动来啦!|青训营第四期',
-    praise: '49',
-    comment: '57',
-  },
-  {
-    id: '3',
-    title: '第五届青训营阅读打卡活动来啦,奖品规则全面升级,快来学习吧',
-    praise: '92',
-    comment: '48',
-  },
-  {
-    id: '4',
-    title: '字节跳动最爱考的前端面试题:CSS基础',
-    praise: '2032',
-    comment: '62',
-  },
-  {
-    id: '5',
-    title: '字节跳动最爱考的前端面试题:JavaScript基础',
-    praise: '3829',
-    comment: '432',
-  },
-])
+// const posts = ref([
+//   {
+//     id: '1',
+//     title: '字节跳动是如何落地微前端的',
+//     praise: '356',
+//     comment: '14',
+//   },
+//   {
+//     id: '2',
+//     title: '记笔记,赢大礼!笔记创作活动来啦!|青训营第四期',
+//     praise: '49',
+//     comment: '57',
+//   },
+//   {
+//     id: '3',
+//     title: '第五届青训营阅读打卡活动来啦,奖品规则全面升级,快来学习吧',
+//     praise: '92',
+//     comment: '48',
+//   },
+//   {
+//     id: '4',
+//     title: '字节跳动最爱考的前端面试题:CSS基础',
+//     praise: '2032',
+//     comment: '62',
+//   },
+//   {
+//     id: '5',
+//     title: '字节跳动最爱考的前端面试题:JavaScript基础',
+//     praise: '3829',
+//     comment: '432',
+//   },
+// ])
+// const { data: posts } = await useLazyFetch(
+//   'http://127.0.0.1:4523/m1/2295980-0-default/api/article/alike'
+// )
+// watch(
+//   posts,
+//   newPosts => {
+//     console.log(newPosts)
+//   },
+//   { immediate: true }
+// )
+interface postType {
+  id: number
+  title: string
+  collect_count: number
+  comment_count: number
+}
+const posts = ref<postType[]>([])
+watchEffect(async () => {
+  const { data } = await useLazyFetch<any>(
+    'http://127.0.0.1:4523/m1/2295980-0-default/api/article/alike'
+  )
+  console.log('data', data)
+  posts.value = data.value?.data
+})
 </script>
 
 <style scoped></style>
