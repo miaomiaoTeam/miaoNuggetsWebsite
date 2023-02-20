419bfd805340f9f9abc5ca18e8513e4b7f3a10d4
luzilin <744883907@qq.com>
2023-02-20 19:49:28 +0800

    feat: 新增文章列表

--- components/post/List.vue
+++ components/post/List.vue
@@ -10,17 +10,90 @@
         >
       </li>
     </ul>
-    <el-divider border-style="solid" />
+    <el-divider />
+    <!-- 文章列表 -->
+    <div v-for="post in postList" :key="post.title" class="px-5 pt-3 mb-[14px]">
+      <!-- 作者 -->
+      <div class="flex items-center">
+        <a href="" class="text-sm">{{ post.author }}</a>
+        <div class="w-[1px] h-[14px] bg-[#e5e6eb] mx-2"></div>
+        <a href="" class="text-sm text-[#909090]">{{ post.time }}</a>
+      </div>
+      <!-- 内容 -->
+      <div>
+        <a href="" class="block text-lg font-bold leading-6 mt-[14px] mb-3">{{
+          post.title
+        }}</a>
+        <a
+          href=""
+          class="block text-sm text-[#86909c] leading-[22px] mb-[14px]"
+          >{{ post.desc }}</a
+        >
+        <!-- 点赞 -->
+        <div class="flex">
+          <div v-for="item in panelList" :key="item.src">
+            <a href="" class="flex items-center mr-8">
+              <img :src="item.src ?? ''" alt="" class="h-4" />
+              <span class="text-sm font-medium text-[#8a93a0]">{{
+                item.data
+              }}</span></a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <el-divider />
   </div>
 </template>
 
 <script setup lang="ts">
+interface panelListOption {
+  type: 'badge' | 'divider' | 'text'
+  src: string
+  data?: number | string
+}
 const sortItems = ref(['推荐', '最新', '热榜'])
+const panelList = ref<panelListOption[]>([
+  { src: '/svg/page_zan.svg', type: 'badge', data: 10 },
+  { src: '/svg/page_comment.svg', type: 'badge', data: 178 },
+  { src: '/svg/page_collect.svg', type: 'text', data: '收藏' },
+])
+const postList = ref([
+  {
+    author: 'luzilin',
+    time: '2022.3.2',
+    title: '5 个 JavaScript 代码优化技巧',
+    desc: '在本文中，将介绍 5 个代码优化的技巧，有助于编写更高效、更优雅的代码。这些技巧包括使用扩展运算符简化代码到使用async/await 处理异步代码等',
+  },
+  {
+    author: 'luzilin',
+    time: '2022.3.2',
+    title: '5 个 JavaScript 代码优化技巧',
+    desc: '在本文中，将介绍 5 个代码优化的技巧，有助于编写更高效、更优雅的代码。这些技巧包括使用扩展运算符简化代码到使用async/await 处理异步代码等',
+  },
+  {
+    author: 'luzilin',
+    time: '2022.3.2',
+    title: '5 个 JavaScript 代码优化技巧',
+    desc: '在本文中，将介绍 5 个代码优化的技巧，有助于编写更高效、更优雅的代码。这些技巧包括使用扩展运算符简化代码到使用async/await 处理异步代码等',
+  },
+  {
+    author: 'luzilin',
+    time: '2022.3.2',
+    title: '5 个 JavaScript 代码优化技巧',
+    desc: '在本文中，将介绍 5 个代码优化的技巧，有助于编写更高效、更优雅的代码。这些技巧包括使用扩展运算符简化代码到使用async/await 处理异步代码等',
+  },
+])
 </script>
 <style scoped>
 .el-divider--horizontal {
   margin: 3px 0;
   background: 0 0;
   border-top: 1px solid #e8eaec;
+}
+.el-button--text {
+  padding: 0px 1px;
+
+  background-color: #c13030;
 }
 </style>
--- pages/post/[...slug].vue
+++ pages/post/[...slug].vue
@@ -10,14 +10,14 @@
         <!-- 文章详情 -->
         <div class="flex-1 postDes:ml-[90px]">
           <postDetailContent>
-            <!-- <ContentRenderer :value="data">
+            <ContentRenderer :value="data">
               <ContentRendererMarkdown
                 ref="markdown"
                 :value="data"
                 class="prose"
                 excerpt
               />
-            </ContentRenderer> -->
+            </ContentRenderer>
           </postDetailContent>
         </div>
         <!-- 右侧栏 -->
@@ -43,45 +43,45 @@
   </div>
 </template>
 <script lang="ts" setup>
-// import { NodeItem } from '@/stores/api/catalog'
-// import type { NodeItems } from '@/stores/api/catalog'
-// import { useCatalogStore } from '@/stores/catalog'
-// // 扁平化数组对象
-// function extractTree(arrs: NodeItems, childs: string) {
-//   const attrList: string[] = Object.keys(arrs[0]) // 存储数组对象的key
-//   if (attrList.includes(childs)) {
-//     // 如果第二参数是children
-//     attrList.splice(attrList.indexOf(childs), 1) // 那么就把attrList的children去掉，此时就变成attrList=["id","pid","name"]
-//   }
-//   const list: NodeItems = []
-//   const getObj = (arrs: NodeItems) => {
-//     arrs.forEach(row => {
-//       const obj = {} as NodeItem
-//       attrList.forEach((item: string) => {
-//         obj[item] = row[item] ? row[item] : ''
-//       })
-//       list.push(obj)
-//       if (row[childs]) {
-//         getObj(row[childs] as unknown as NodeItems) // 递归
-//       }
-//     })
-//     return list
-//   }
-//   return getObj(arrs)
-// }
-// const catalog = useCatalogStore()
-// const route = useRoute()
-// const markdown = ref()
-// const tocs = ref<NodeItems>([])
-// const { data } = await useAsyncData<any>('page-data', () =>
-//   queryContent('/docs/standards/git').findOne()
-// )
-// onMounted(() => {
-//   tocs.value = [...markdown.value.value.body.toc.links]
-//   tocs.value = extractTree(tocs.value, 'children')
-//   catalog.update_doc_catalog(tocs.value)
-//   // console.log('tocs.value', tocs.value)
-//   // console.log('markdown.value', markdown.value)
-// })
-// console.log('route.params.id', route.params.slug)
+import { NodeItem } from '@/stores/api/catalog'
+import type { NodeItems } from '@/stores/api/catalog'
+import { useCatalogStore } from '@/stores/catalog'
+// 扁平化数组对象
+function extractTree(arrs: NodeItems, childs: string) {
+  const attrList: string[] = Object.keys(arrs[0]) // 存储数组对象的key
+  if (attrList.includes(childs)) {
+    // 如果第二参数是children
+    attrList.splice(attrList.indexOf(childs), 1) // 那么就把attrList的children去掉，此时就变成attrList=["id","pid","name"]
+  }
+  const list: NodeItems = []
+  const getObj = (arrs: NodeItems) => {
+    arrs.forEach(row => {
+      const obj = {} as NodeItem
+      attrList.forEach((item: string) => {
+        obj[item] = row[item] ? row[item] : ''
+      })
+      list.push(obj)
+      if (row[childs]) {
+        getObj(row[childs] as unknown as NodeItems) // 递归
+      }
+    })
+    return list
+  }
+  return getObj(arrs)
+}
+const catalog = useCatalogStore()
+const route = useRoute()
+const markdown = ref()
+const tocs = ref<NodeItems>([])
+const { data } = await useAsyncData<any>('page-data', () =>
+  queryContent('/docs/standards/git').findOne()
+)
+onMounted(() => {
+  tocs.value = [...markdown.value.value.body.toc.links]
+  tocs.value = extractTree(tocs.value, 'children')
+  catalog.update_doc_catalog(tocs.value)
+  // console.log('tocs.value', tocs.value)
+  // console.log('markdown.value', markdown.value)
+})
+console.log('route.params.id', route.params.slug)
 </script>
