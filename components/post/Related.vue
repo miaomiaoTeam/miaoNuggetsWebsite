5864cb4d1298c3997673c94b49eec9cce085ac0f
luzilin <744883907@qq.com>
2023-02-21 16:10:17 +0800

    fix : 相关文章联动

--- components/post/Related.vue
+++ components/post/Related.vue
@@ -5,10 +5,10 @@
         <div>相关文章</div>
       </template>
       <div v-for="post in post_List?.data" :key="post.id" class="mb-[8px]">
-        <a
-          href=""
+        <NuxtLink
+          :to="post.content_path"
           class="text-[14px] font-normal text-[#252933] leading-[22px] hover:text-[#1e80ff]"
-          >{{ post.title }}</a
+          >{{ post.title }}</NuxtLink
         >
         <div
           class="text-[#8a919f] text-[14px] font-normal leading-[22px] mt-[4px]"
@@ -21,65 +21,14 @@
 </template>
 
 <script setup lang="ts">
-// const posts = ref([
-//   {
-//     id: '1',
-//     title: '字节跳动是如何落地微前端的',
-//     praise: '356',
-//     comment: '14',
-//   },
-//   {
-//     id: '2',
-//     title: '记笔记,赢大礼!笔记创作活动来啦!|青训营第四期',
-//     praise: '49',
-//     comment: '57',
-//   },
-//   {
-//     id: '3',
-//     title: '第五届青训营阅读打卡活动来啦,奖品规则全面升级,快来学习吧',
-//     praise: '92',
-//     comment: '48',
-//   },
-//   {
-//     id: '4',
-//     title: '字节跳动最爱考的前端面试题:CSS基础',
-//     praise: '2032',
-//     comment: '62',
-//   },
-//   {
-//     id: '5',
-//     title: '字节跳动最爱考的前端面试题:JavaScript基础',
-//     praise: '3829',
-//     comment: '432',
-//   },
-// ])
-// const { data: posts } = await useLazyFetch(
-//   'http://127.0.0.1:4523/m1/2295980-0-default/api/article/alike'
-// )
-// watch(
-//   posts,
-//   newPosts => {
-//     console.log(newPosts)
-//   },
-//   { immediate: true }
-// )
-// interface postType {
-//   id: number
-//   title: string
-//   collect_count: number
-//   comment_count: number
-// }
-// const posts = ref<postType[]>([])
-// watchEffect(async () => {
-//   const { data } = await useLazyFetch<any>(
-//     'http://127.0.0.1:4523/m1/2295980-0-default/api/article/alike'
-//   )
-//   // console.log('data', data)
-//   posts.value = data.value?.data
-// })
+import { storeToRefs } from 'pinia'
+import { usePostStore } from '@/stores/post/post_detail'
+const postStore = usePostStore()
+const { post_id } = storeToRefs(postStore)
+
 const { data: post_List } = await useAsyncData('post_list', () =>
   $fetch('/api/article/alike', {
-    query: { id: 3 },
+    query: { id: post_id.value },
   })
 )
 </script>
--- stores/post/post_detail.ts
+++ stores/post/post_detail.ts
@@ -2,7 +2,6 @@ import { defineStore } from 'pinia'
 export const usePostStore = defineStore('post_detail', () => {
   const post_id = ref(0)
   function setPostId(id: number) {
-    console.log('id', id)
     post_id.value = id
   }
 
